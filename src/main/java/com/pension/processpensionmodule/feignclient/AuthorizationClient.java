47852c8626a74b2781dab8d96c5ae8bed8c075c8
MrPaul98 <sayandippaul1998@gmail.com>
2022-08-19 15:47:09 +0530

    appspec java change

--- src/main/java/com/pension/processpensionmodule/feignclient/AuthorizationClient.java
+++ src/main/java/com/pension/processpensionmodule/feignclient/AuthorizationClient.java
@@ -16,7 +16,7 @@ import com.pension.processpensionmodule.model.AuthorizationResponse;
  *
  */
 //@FeignClient(name = "Authorization-Service", url = "http://localhost:8400")
-@FeignClient(name = "Authorization-Service", url = "http://3.141.18.28:8400")	
+@FeignClient(name = "Authorization-Service", url = "http://18.217.222.29:8400")	
 public interface AuthorizationClient {
 
 	@GetMapping(value = "/api/authorize/{token}")
